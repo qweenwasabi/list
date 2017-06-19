3de1f510ddb80d26bcb8be65053a4aa04b8e03ba
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-06-18 21:15:39 -0700

    input css down

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+function postShoe (year, name, size, image) {
+  this.year: year;
+  this.name: name;
+  this.size: size;
+  this.image: image
+
+  
+}
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const app = express()
-var path = require('path');
 
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
